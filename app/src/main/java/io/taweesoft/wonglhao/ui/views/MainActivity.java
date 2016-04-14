0e02307ad3e5b791dae2cc451d92300ffe0d3f8e
TAWEESOFT <taweesoft@gmail.com>
2016-04-14 19:27:23 +0700

    [#117424851] Added left menu overview layout

--- app/src/main/java/io/taweesoft/wonglhao/ui/views/MainActivity.java
+++ app/src/main/java/io/taweesoft/wonglhao/ui/views/MainActivity.java
@@ -30,6 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         menuDrawer = MenuDrawer.attach(this);
-        
+        menuDrawer.setContentView(R.layout.activity_main);
     }
 }
