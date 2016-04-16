83d94c28bbdd14c75a91e9b8c43f126d2a10e02d
taweesoft <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-04-16 18:55:10 +0700

    [#117544253] Remove background color from AppTheme

---
+++ app/src/main/java/io/taweesoft/wonglhao/ui/adapters/SearchBarAdapter.java
@@ -0,0 +1,66 @@
+package io.taweesoft.wonglhao.ui.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import io.taweesoft.wonglhao.R;
+import io.taweesoft.wonglhao.models.Bar;
+
+/**
+ * Created by TAWEESOFT on 4/16/16 AD.
+ */
+public class SearchBarAdapter extends RecyclerView.Adapter<SearchBarAdapter.ViewHolder> {
+
+    private Context context;
+    private List<Bar> barList;
+
+    public SearchBarAdapter(List<Bar> barList) {
+        this.barList = barList;
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.tvName) public TextView tvName;
+        @Bind(R.id.tvCheckedIn) public TextView tvCheckedIn;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this,itemView);
+        }
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        this.context = parent.getContext();
+        View v = LayoutInflater.from(context).inflate(R.layout.search_item_layout,null);
+        final ViewHolder viewHolder = new ViewHolder(v);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = viewHolder.getPosition();
+                // TODO: 4/16/16 AD Open bar activity.
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Bar bar = barList.get(position);
+        holder.tvName.setText(bar.getName());
+        holder.tvCheckedIn.setText(bar.getChecked_in().size() + " " + context.getString(R.string.peopleWereHere));
+    }
+
+    @Override
+    public int getItemCount() {
+        return barList.size();
+    }
+}
