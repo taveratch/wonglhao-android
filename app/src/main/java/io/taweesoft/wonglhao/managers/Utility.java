7de5dbf9f2fbf6e1b8b78428c40a8bb31ce3b279
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-04-18 10:07:17 +0700

    [#117424971] Check ACCESS_FINE_LOCATION when start activity

--- app/src/main/java/io/taweesoft/wonglhao/managers/Utility.java
+++ app/src/main/java/io/taweesoft/wonglhao/managers/Utility.java
@@ -1,6 +1,12 @@
 package io.taweesoft.wonglhao.managers;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 /**
@@ -10,5 +16,14 @@ public class Utility {
 
     public static Toast showToastDialog(Context context, String message , boolean isShowLong) {
         return Toast.makeText(context , message, isShowLong ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+    }
+
+    public static void canAccessLocation(Activity activity) {
+        final int REQUEST_CODE_ASK_PERMISSIONS = 123;
+        int hasWriteContactsPermission = activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION);
+        if (hasWriteContactsPermission != PackageManager.PERMISSION_GRANTED) {
+            activity.requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_ASK_PERMISSIONS);
+        }
     }
 }
--- app/src/main/java/io/taweesoft/wonglhao/ui/views/LoginActivity.java
+++ app/src/main/java/io/taweesoft/wonglhao/ui/views/LoginActivity.java
@@ -1,7 +1,9 @@
 package io.taweesoft.wonglhao.ui.views;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -33,6 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        Utility.canAccessLocation(this); //check access loaction permission.
     }
 
     @OnClick(R.id.btnSignin)
